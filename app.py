b4bf549ab65007c02a6e57d4fb426aa413760ac1
naderelzinyy <naderelziny805@gmail.com>
2022-04-30 18:54:18 +0300

    adding receive emails feature

--- app.py
+++ app.py
@@ -8,18 +8,21 @@ from email import encoders
 from contextlib import contextmanager
 import re
 from tkinter import filedialog
+import easyimap as imap
+from abc import ABC, abstractmethod
 
 
 class MailClient:
-    def __init__(self, port: int, host: str):
+    def __init__(self, port: int, smtp_host: str, imap_host: str):
         self.port = port  # SSL Port
-        self.host = host
+        self.smtp_host = smtp_host
+        self.imap_host = imap_host
         self.__connection_context = ssl.create_default_context()
 
     @contextmanager
     def ssl_connection(self, email: str, password: str) -> None:
         # Create a secure SSL context
-        with smtp.SMTP_SSL(self.host, self.port, context=self.__connection_context) as mail_server:
+        with smtp.SMTP_SSL(self.smtp_host, self.port, context=self.__connection_context) as mail_server:
             try:
                 # mail_server.connect(email, self.port)
                 mail_server.login(email, password)
@@ -35,11 +38,23 @@ class MailClient:
     @contextmanager
     def tls_connection(self, email: str, password: str) -> None:
         try:
-            server = smtp.SMTP(host=self.host, port=self.port)
+            server = smtp.SMTP(host=self.smtp_host, port=self.port)
             server.ehlo()
             server.starttls()
             server.ehlo()
             server.login(email, password)
+            yield server
+        except Exception as e:
+            print(e)
+        else:
+            print('Connection successfully established')
+        finally:
+            server.close()
+
+    @contextmanager
+    def imap_connection(self, email: str, password: str):
+        try:
+            server = imap.connect(self.imap_host, email, password)
             yield server
         except Exception as e:
             print(e)
@@ -51,15 +66,16 @@ class MailClient:
 
 class OutlookClient(MailClient):
     def __init__(self):
-        super().__init__(port=587, host="smtp.office365.com")
+        super().__init__(port=587, smtp_host="smtp.office365.com",imap_host="imap-mail.outlook.com")
 
 
 class YahooMailClient(MailClient):
     pass
 
+
 class GmailClient(MailClient):
     def __init__(self):
-        super().__init__(port=465, host="smtp.gmail.com")
+        super().__init__(port=465, smtp_host="smtp.gmail.com",imap_host="imap.gmail.com")
 
 
 class YandexClient(MailClient):
@@ -73,11 +89,38 @@ class YandexClient(MailClient):
     """
 
     def __init__(self):
-        super().__init__(port=465, host="smtp.yandex.com")
+        super().__init__(port=465, smtp_host="smtp.yandex.com",imap_host="imap.yandex.com")
 
 
-class Email:
+class Email(ABC):
+    mail_clients = {
+        'outlook': OutlookClient,
+        'hotmail': OutlookClient,
+        'yahoo': YahooMailClient,
+        'gmail': GmailClient,
+        'yandex': YandexClient
+    }
+
+    @abstractmethod
+    def set_domain_name(self):
+        pass
+
+    @abstractmethod
+    def set_password(self):
+        pass
+
+    @abstractmethod
+    def set_mail_client(self):
+        pass
+
+    @abstractmethod
+    def login(self):
+        pass
+
+
+class EmailSender(Email):
     def __init__(self):
+        super().__init__()
         self.__subject = None
         self.__from = None
         self.__to = None
@@ -135,27 +178,20 @@ class Email:
         mail_content.attach(self.set_attachment())
         return mail_content
 
-    def __set_domain_name(self) -> str:
+    def set_domain_name(self) -> str:
         domain = re.search("[@-][\w]+", self.__from).group()
         domain_name = domain[1:]
         return domain_name
 
-    def __set_mail_client(self):
-        mail_clients = {
-            'outlook': OutlookClient,
-            'hotmail': OutlookClient,
-            'yahoo': YahooMailClient,
-            'gmail': GmailClient,
-            'yandex': YandexClient
-        }
-        selected_mail_client = self.__set_domain_name()
-        self.__mail_client = mail_clients.get(selected_mail_client)
+    def set_mail_client(self):
+
+        selected_mail_client = self.set_domain_name()
+        self.__mail_client = super().mail_clients.get(selected_mail_client)
 
     def __send_execute(self) -> None:
+        mail_client_connection = self.__mail_client().ssl_connection
         if self.__mail_client == OutlookClient:
             mail_client_connection = self.__mail_client().tls_connection
-        else:
-            mail_client_connection = self.__mail_client().ssl_connection
 
         with mail_client_connection(self.__from, self.__password) as connection:
             try:
@@ -168,7 +204,7 @@ class Email:
         """
         self.set_sender()
         self.set_password()
-        self.__set_mail_client()
+        self.set_mail_client()
 
     def send(self) -> None:
         self.set_recipient()
@@ -177,55 +213,51 @@ class Email:
         self.__send_execute()
 
 
-# def send_email():
-#     # mail_password = getpass.getpass("Password: ")
-#     mail_password = 'PythonEmailTest'
-#     sender_email = 'zidev.py@yahoo.com'
-#     receiver_email = 'naderelziny805@gmail.com'
-#     # message = 'Hello, this is a test email'
-#     message = MIMEMultipart('alternative')
-#     message["Subject"] = "Multipart Test2"
-#     message["From"] = sender_email
-#     message["To"] = receiver_email
-#     # text = ""
-#     html = """
-#     <html>
-#         <body>
-#             <h1 style="color: red;">Hello</h1><br>
-#             <p>This is zidev.py trying to send emails with python</p>
-#         </body>
-#     </html>
-#
-#     """
-#
-#     # part1 = MIMEText(text, 'plain')
-#     part2 = MIMEText(html, 'html')
-#     file_name = 'DHLdocument2.pdf'
-#
-#     with open(file_name, 'rb') as attachment:
-#         part = MIMEBase('application', 'octet-stream')
-#         part.set_payload(attachment.read())
-#
-#     encoders.encode_base64(part)
-#     part.add_header(
-#         "Content-Disposition",
-#         "attachment; filename= Our Pdf Document",
-#     )
-#
-#     # message.attach(part1)
-#     message.attach(part)
-#     message.attach(part2)
-#
-#     with server_connection(mail_password) as connection:
-#         try:
-#             connection.sendmail(sender_email, receiver_email, message.as_string())
-#         except RuntimeError("generator didn't yield"):
-#             pass
+class EmailReceiver(Email):
+    def __init__(self):
+        super().__init__()
+        self.__email = None
+        self.__password = None
+        self.__mail_client = None
+
+    def set_email(self):
+        self.__email = str(input("Enter Email: "))
+
+    def set_password(self):
+        self.__password = str(input("Enter Password: "))
+
+    def set_domain_name(self):
+        domain = re.search("[@-][\w]+", self.__email).group()
+        domain_name = domain[1:]
+        return domain_name
+
+    def set_mail_client(self):
+        selected_mail_client = self.set_domain_name()
+        self.__mail_client = super().mail_clients.get(selected_mail_client)
+
+    def login(self):
+        self.set_email()
+        self.set_password()
+        self.set_mail_client()
+
+    def receive(self) -> None:
+        mail_client_connection = self.__mail_client().imap_connection
+        with mail_client_connection(self.__email, self.__password) as connection:
+            connection.listids()
+            email = connection.mail(connection.listids()[3])
+            # for title
+            print(email.title)
+            # for the sender’s email address
+            print(email.from_addr)
+            # for the main content of the email
+            print(email.body)
+            # for any type of attachment
+            print(email.attachments)
 
 
 if __name__ == '__main__':
     try:
-        email = Email()
+        email = EmailSender()
         email.login()
         email.send()
     except RuntimeError as e:
