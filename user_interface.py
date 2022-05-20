3b7c53becbe315b9a9e8fee63a4e87cd4474d06a
naderelzinyy <naderelziny805@gmail.com>
2022-05-20 23:03:30 +0300

    Cleaning code (AppInterface Class)

--- user_interface.py
+++ user_interface.py
@@ -12,14 +12,7 @@ class AppInterface:
         self.account_manager = app_file.EmailAccountManager()
         self.email_sender = app_file.EmailSender()
         self.email_receiver = app_file.EmailReceiver()
-        self.choice = None
         self.selected_account = None
-        self.scenarios = {
-            "4": self.account_manager.login,
-            "5": self.send_execute,
-            "6": self.receive_execute,
-        }
-
         self.commands = {
             "1": self.login.execute,
             "2": self.register.execute,
@@ -38,7 +31,7 @@ class AppInterface:
         self.cmd = str(input("1- Sign in \n2- Register \n"))
         if self.cmd == "1" or self.cmd == "2":
             self.commands.get(self.cmd)()
-        elif self.choice == "quit":
+        elif self.cmd == "quit":
             self.commands.get("quit")()
         else:
             print("Choose 1 or 2")
@@ -48,23 +41,9 @@ class AppInterface:
             self.view_accounts()
 
     @staticmethod
-    def welcome_page():
+    def welcome_page() -> None:
         welcome_text = Figlet(font="starwars")
         print(colored(welcome_text.renderText('MY     MAIL     SYSTEM'), 'green'))
-
-    # def main_page(self) -> None:
-    #     self.view_accounts()
-    #     print("\n")
-    #     self.choice = str(input("3- View email clients \n4- Add new email client \n5- Send an email \n6- Open inbox \n"))
-    #     if self.choice == "3":
-    #         self.view_accounts()
-    #     elif self.choice == "4":
-    #         self.scenarios.get(self.choice)(username=self.login.username)
-    #     elif self.choice == "5" or self.choice == "6":
-    #         self.scenarios.get(self.choice)()
-    #     else:
-    #         print("Wrong value.\nPlease choose again.")
-    #         self.main_page()
 
     def choose_mail_account_page(self) -> None:
         accounts = self.commands.get("3")(username=self.login.username)
@@ -94,7 +73,7 @@ class AppInterface:
             print(accounts.index(account)+1, "- "+account[0], " || ", status)
 
     @staticmethod
-    def help():
+    def help() -> None:
         print("send -> sends an email\ninbox -> opens the inbox\nadd acc -> adds an email account\nlogout -> logs the user out\nquit -> closes the app")
 
     @staticmethod
@@ -103,7 +82,7 @@ class AppInterface:
         register.execute()
 
     @staticmethod
-    def exit_app():
+    def exit_app() -> None:
         print("Thanks for using our app")
         exit()
 
